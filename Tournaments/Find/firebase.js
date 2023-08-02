30c173b49bc30de0d10e1afbc96dc8c41e5baab5
Vivekanand R <75977224+vivek-unni@users.noreply.github.com>
2023-08-03 01:57:04 +0530

    Update firebase.js

--- Tournaments/Find/firebase.js
+++ Tournaments/Find/firebase.js
@@ -19,46 +19,70 @@ const analytics = getAnalytics(app);
 const auth = getAuth();
 const db = getFirestore(app);
 
-auth.onAuthStateChanged(async function (user) {
-  if (user) 
-  {
-    // Read data from the 'hosttournaments' collection
-    const tournamentsRef = collection(db, 'hosttournamentclubs');
-    getDocs(tournamentsRef)
+const cardsContainer = document.querySelector(".row-cols-1");
+const allFilterButton = document.querySelector("[data-filter='all']");
+const cricketFilterButton = document.querySelector("[data-filter='cricket']");
+const footballFilterButton = document.querySelector("[data-filter='football']");
+
+// Function to filter tournaments based on their "tournamentSport" field
+function filterTournaments(sportFilter) {
+  const tournamentsRef = collection(db, 'hosttournamentclubs');
+  getDocs(tournamentsRef)
     .then((querySnapshot) => {
-      // Generate HTML cards dynamically based on the retrieved data
-      var cardsContainer = document.querySelector(".row-cols-1");
       cardsContainer.innerHTML = "";
       querySnapshot.forEach((doc) => {
-        var tournament = doc.data();
-        var card = document.createElement("div");
-        card.className = "col";
-        console.log(doc.id);
-        const docid=doc.id;
-        //sessionStorage.setItem("tournamentid", docid);
-        card.innerHTML = `
-          <div class="card">
-            <img src="${tournament.posterURL}" class="card-img-top" alt="${tournament.name} Poster">
-            <div class="card-body">
-              <h5 class="card-title">${tournament.tournamentName}</h5>
-              <p class="card-text">Venue: ${tournament.place}</p>
-              <p class="card-text">Date: ${tournament.date}</p>
-              <p class="card-text">Prize pool: ${tournament.prizePool}</p>
-              <p class="card-text">${tournament.conditions}</p>
-              <div class="entry-fee-box">
-                <span class="entry-fee-tag">Entry Fee</span>
-                <span class="entry-fee">${tournament.entryFee}</span>
+        const tournament = doc.data();
+        if (sportFilter === 'all' || tournament.tournamentSport === sportFilter) {
+          var card = document.createElement("div");
+          card.className = "col";
+          console.log(doc.id);
+          const docid = doc.id;
+          //sessionStorage.setItem("tournamentid", docid);
+          card.innerHTML = `
+            <div class="card">
+              <img src="${tournament.posterURL}" class="card-img-top" alt="${tournament.name} Poster">
+              <div class="card-body">
+                <h5 class="card-title">${tournament.tournamentName}</h5>
+                <p class="card-text">Venue: ${tournament.place}</p>
+                <p class="card-text">Date: ${tournament.date}</p>
+                <p class="card-text">Prize pool: ${tournament.prizePool}</p>
+                <p class="card-text">${tournament.conditions}</p>
+                <div class="entry-fee-box">
+                  <span class="entry-fee-tag">Entry Fee</span>
+                  <span class="entry-fee">${tournament.entryFee}</span>
+                </div>
+                <button class="glow-on-hover" type="button" onclick="window.location.href='/Tournaments/Find/apply/apply.html?tournamentId=${docid}';">
+                  Apply Now</button>
               </div>
-              <button class="glow-on-hover" type="button" onclick="window.location.href='/Tournaments/Find/apply/apply.html?tournamentId=${docid}';">
-                Apply Now</button>
             </div>
-          </div>
-        `;
-        cardsContainer.appendChild(card);
+          `;
+          cardsContainer.appendChild(card);
+        }
       });
     })
     .catch((error) => {
       console.log("Error getting tournaments: ", error);
     });
+}
+
+// Event listener for the "All" filter button
+allFilterButton.addEventListener("click", function () {
+  filterTournaments("all");
+});
+
+// Event listener for the "Cricket" filter button
+cricketFilterButton.addEventListener("click", function () {
+  filterTournaments("Cricket");
+});
+
+// Event listener for the "Football" filter button
+footballFilterButton.addEventListener("click", function () {
+  filterTournaments("Football");
+});
+
+auth.onAuthStateChanged(async function (user) {
+  if (user) {
+    // Load all tournaments initially (when the page loads)
+    filterTournaments("all");
   }
 });
